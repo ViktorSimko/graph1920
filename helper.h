c7dde78b67db89372e236b21e580112e6731791d
unknown <bgeri74@gmail.com>
2019-03-15 20:52:47 +0100

    Did some minor modifications

--- helper.h
+++ helper.h
@@ -1,10 +1,15 @@
 #pragma once
 class Point2D {
 public:
-	int x;
-	int y;
+	double x;
+	double y;
 
-	Point2D(int x, int y) {
+	Point2D() {
+		x = 0;
+		y = 0;
+	}
+
+	Point2D(double x, double y) {
 		this->x = x;
 		this->y = y;
 	}
@@ -12,11 +17,17 @@ public:
 
 class Point3D {
 public:
-	int x;
-	int y;
-	int z;
+	double x;
+	double y;
+	double z;
 
-	Point3D(int x, int y, int z) {
+	Point3D() {
+		x = 0;
+		y = 0;
+		z = 0;
+	}
+
+	Point3D(double x, double y, double z) {
 		this->x = x;
 		this->y = y;
 		this->z = z;
@@ -39,3 +50,31 @@ public:
 		this->b = b;
 	}
 };
+
+
+std::vector<std::string> wordsInLine(std::string line, char separator=' ') {
+	std::vector<std::string> words;
+
+	std::string word = "";
+
+	for (auto c : line)
+	{
+		if (c == separator)
+		{
+			if (word != "") {
+				words.push_back(word);
+			}
+			word = "";
+		}
+		else
+		{
+			word = word + c;
+		}
+	}
+
+	if (word != "") {
+		words.push_back(word);
+	}
+
+	return words;
+}
